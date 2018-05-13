dc34829684c59a20fd3db237ea650af45679f8e2
Ryszard Kubinski <ryszard.kubinski@mail.mcgill.ca>
2018-05-13 14:16:47 -0400

    -working with asynchronous IO

---
+++ arbitrage_new/sub.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+#this converts our BASE into dollars
+
+def coinmarketcapPriceFetch(symbol):
+
+    idx = symbol.find('/')
+    subs = symbol[idx+1:]
+
+    id=""
+
+    ListingsURL="https://api.coinmarketcap.com/v2/listings/"
+    TickerURL="https://api.coinmarketcap.com/v2/ticker/"
+    request=requests.get(ListingsURL)
+    data=request.json()['data']
+
+    for x in data:
+        sym=x['symbol']
+
+        if subs == sym:
+
+            id=x['id']
+
+    TickerURL+=str(id)
+
+    request=requests.get(TickerURL)
+    data=request.json()['data']['quotes']['USD']
+
+    price=data['price']
+
+    return price
+
+
