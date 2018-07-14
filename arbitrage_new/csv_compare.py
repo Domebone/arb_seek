84b5d68d710e270d5e849abb80afeb020a521159
Ryszard Kubinski <ryszard.kubinski@mail.mcgill.ca>
2018-07-14 18:50:06 -0400

    made sure arb opps for same currency from different days would still show up

--- arbitrage_new/csv_compare.py
+++ arbitrage_new/csv_compare.py
@@ -16,9 +16,14 @@ with open("master.csv", "r", newline='') as m:
                 for row in master_reader:
 
                     if row_report[1] in row and row_report[2] in row and row_report[4] in row:
-                        match_counter += 1
+                        if row_report[0][8]==row[0][8] and row_report[0][9]==row[0][9]: #some arb opps might pop up on different days
+                                                                                        #this helps us not exclude an arb opp that occurred 3 days ago
+                            match_counter += 1
+
+
 
                 if match_counter < 1:
+                    print(row_report)
                     writer.writerow(row_report)
                 match_counter = 0
 
@@ -33,4 +38,4 @@ with open("final_report.csv","r") as f:
         writer=csv.writer(m, delimiter='\t')
 
         for row in reader:
-            writer.writerow(row)            #we append all the new stuff to the master sheet.
+            writer.writerow(row)            #we append all the new stuff to the master sheet.'''
